108eb84d41f8e7d1e7e62bcee20022f8116a621b
dgumenyuk <gumenyuk98@gmail.com>
2020-03-01 21:46:05 -0500

    Update wordCount.py

--- Result-1_version2/wordCount.py
+++ Result-1_version2/wordCount.py
@@ -1,3 +1,4 @@
+
 import nltk
 from nltk.corpus import stopwords
 from pyspark import SparkContext
@@ -7,17 +8,11 @@ import string
 
 nltk.download('stopwords')
 
-punc = ' !"#$%&\'()*+,./:;<=>?@[\\]^_`{|}~ '
-
 def clean_str(x):
     x = x.lower()
     x = x.replace('--',' ')
     x = x.replace('“', "")
     clean_str = re.sub(r'^\W+|\W+$', '', x)
-   # x = x.replace('\'', "")
-   # table = str.maketrans('', '', string.punctuation)
-   # clean_str = x.translate(table)
-   # clean_str = x.strip(punc)
     return re.split(r'\W+',clean_str)
 
 if __name__ == '__main__':
@@ -30,17 +25,16 @@ if __name__ == '__main__':
         stopWords = set(stopwords.words('english'))
 
         rdd = sc.textFile('result.txt')
-        rdd = rdd.filter(lambda x: len(x) > 0)
+        rdd = rdd.filter(lambda x: len(x) > 0) 
 
         rdd = rdd.flatMap(lambda x: clean_str(x))
         rdd = rdd.filter(lambda x: x not in stopWords)
 
-#        print(rdd.take(3000))
         rdd = rdd.map(lambda x: (x, 1))
         rdd = rdd.reduceByKey(lambda x, y: x + y)
-#        print(rdd.take(5))
-        rdd = rdd.map(lambda x: (x[1], x[0]))
- #       print(rdd.take(5))
+        rdd = rdd.map(lambda x: (x[1], x[0])) # switch values and keys to sort by the largest number of occurances
         rdd = rdd.sortByKey(False)
         print(rdd.take(20))
+
+
 
